b7f6308a98524cd3e68115f65ee17aa88e59ede3
HeoSinYoung <hechgaya@naver.com>
2019-10-26 23:04:37 +0900

    GameFramework.py 추가
    GameFramework.py 추가

---
+++ GameFramework.py
@@ -0,0 +1,71 @@
+class GameState:
+    def __int__(self, state):
+        self.Enter = state.Enter
+        self.Exit = state.Exit
+        self.Pause = state.Pause
+        self.Handle_Events = state.Handle_Events
+        self.Update = state.Update
+        self.Render = state.Render
+        pass
+
+
+running = None
+stack = None
+
+
+def Change_State(state):
+    global stack
+
+    if len(stack) > 0:
+        stack[-1].Exit()
+        stack.pop()
+
+    stack.append(state)
+    state.Enter()
+
+
+def Push_State(state):
+    global stack
+
+    if len(stack) > 0:
+        stack[-1].pause
+
+    stack.append(state)
+    state.Enter()
+
+
+def Pop_State():
+    global stack
+
+    if len(stack) > 0:
+        stack[-1].Exit()
+        stack.pop()
+
+    if len(stack) > 0:
+        stack[-1].Resum()
+
+
+def Quit():
+    global running
+    running = False
+
+
+def Run(start_state):
+    global running, stack
+
+    running = True
+    stack = [start_state]
+
+    start_state.Enter()
+
+    while running:
+        stack[-1].Handle_Events()
+        stack[-1].Update()
+        stack[-1].Render()
+
+    while len(stack) > 0:
+        stack[-1].Exit()
+        stack.pop()
+
+
+
