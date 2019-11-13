aada55c73c5cddef20cda4561f77a6c08c30ac90
HeoSinYoung <hechgaya@naver.com>
2019-11-13 17:07:05 +0900

    Player Lazer 클래스 추가중.
    Player Lazer 클래스 추가중.

---
+++ PlayerLazer.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+class CPlayerLazer:
+    def __init__(self):
+        
+        pass
+
+    def Handle_Events(self):
+        pass
+
+    def Update(self):
+        pass
+
+    def Render(self):
+        pass
+
+    pass
