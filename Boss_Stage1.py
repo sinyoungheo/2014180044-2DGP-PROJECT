73396b60ebd814394f09715c980bf085cf9907e5
HeoSinYoung <hechgaya@naver.com>
2019-11-24 16:34:47 +0900

    Stage1 보스 추가 중.
    Stage1 보스 추가 중.

---
+++ Boss_Stage1.py
@@ -0,0 +1,22 @@
+from pico2d import *
+import random
+import GameFramework
+
+
+class CBossStage1:
+    def __init__(self):
+        pass
+
+    def Handle_Events(self):
+        pass
+
+    def Update(self):
+        pass
+
+    def Render(self):
+        pass
+
+    ######################################
+    
+
+    pass
